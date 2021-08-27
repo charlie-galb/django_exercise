f4c3662d9d6b94b342707ef1e4fbee22850e7332
Charlie Galbraith <charlie.galbraith@clicktravel.com>
2021-08-27 10:58:44 +0100

    Add ingredient model

--- app/core/models.py
+++ app/core/models.py
@@ -8,3 +8,15 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient object"""
+    name = models.CharField(max_length=255)
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
